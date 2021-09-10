956ff9e1b5bd24fd79bfad39223ccad0861092ed
Kristin <kristinponsonby@gmail.com>
2021-09-10 18:05:10 -0400

    Add show page

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -8,23 +8,45 @@ class Recipe {
     }
 
 
-    renderCard = () => {
-        // destructuring: 'this' is the passed instance of a recipe
-        const { name, category, ingredients, imageUrl, id } = this.data
-         document.querySelector(".recipe-container").innerHTML +=
-          `<div class="recipe-card">
-              <img src=${imageUrl} alt=${name}/>
-              <p class="title">${name}</p>
-              <p>${category}</p>
-              <p>${ingredients}</p>
+    renderShow = () => {
+        const { name, category, ingredients, imageUrl} = this.data
+        document.getElementById("main").innerHTML = `
+        <div class="show">
+            <img src="${imageUrl}" alt=${name}/>
+            <p class="title">${name}</p>
+            <p>Ingredients: ${ingredients}</p>
+            <p>Category: ${category}</p>
           </div>`
     }
 
-    static renderIndex(){
+
+    renderCard = () => {
+        // destructuring: 'this' is the passed instance of a recipe
+        const { name, category, ingredients, imageUrl, id } = this.data
+         document.getElementById("recipe-container").innerHTML +=
+          `<div class="recipe-card" data-id=${id}>
+              <img src=${imageUrl} alt=${name}/>
+              <p class="title">${name}</p>
+              <p>Ingredients: ${ingredients}</p>
+              <p>Category: ${category}</p>
+          </div>`
+    }
+
+    static find = (id) => this.all.find(recipe => recipe.data.id ==id)
+
+    static handleIndexClick = (e) => {
+        if (e.target.tagName == "IMG" || e.target.classList.contains("title")){
+            const id = e.target.closest(".recipe-card").dataset.id
+            this.find(id).renderShow()
+        }
+    }
+
+    static renderIndex= () => {
         const recipeContainer = document.createElement("div") 
-        recipeContainer.classList.add("recipe-container")
+        recipeContainer.id= "recipe-container"
         document.getElementById("main").appendChild(recipeContainer)
         this.all.forEach(recipe => recipe.renderCard())
+        recipeContainer.addEventListener("click", this.handleIndexClick)
     }
 
      static getRecipes = () => {
