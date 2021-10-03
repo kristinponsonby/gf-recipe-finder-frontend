965303de0537fe70ce229bd49d17cbe54140a739
Kristin <kristinponsonby@gmail.com>
2021-10-03 10:36:01 -0400

    Remove console.log

--- src/services/ApiService.js
+++ src/services/ApiService.js
@@ -12,9 +12,7 @@ class ApiService {
 
     //Fetch call 2
     createRecipe = (newRecipe) => {
-        console.log(newRecipe)
-        console.log(user)
-        // Make sure when a user posts a new recipe, the user id is first added to that new recipe
+    // Make sure when a user posts a new recipe, the user id is first added to that new recipe
     newRecipe.user_id = user.id
      return fetch(this.api + "/recipes", {
       method: 'POST', // or 'PUT'
@@ -28,7 +26,6 @@ class ApiService {
 
     //Fetch call 3
     findOrCreateUser = (username) => {
-        console.log(username)
         return fetch(this.api + "/users", {
             method: 'POST', // or 'PUT'
             headers: {
